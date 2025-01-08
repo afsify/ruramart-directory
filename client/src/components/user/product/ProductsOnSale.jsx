0a85dba957e9553d8df4ee3e5dd6a7a86f6c8a8e
AFSAL333 <afsalafs8008@gmail.com>
2025-01-09 00:16:24 +0530

    product sale updated

--- client/src/components/user/product/ProductsOnSale.jsx
+++ client/src/components/user/product/ProductsOnSale.jsx
@@ -1,42 +1,49 @@
+const newArrOne =
+  "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg";
+const newArrTwo =
+  "https://cdn.pixabay.com/photo/2020/06/16/10/30/camera-5305154_640.jpg";
+const newArrThree =
+  "https://cdn.olaelectric.com/sites/evdp/pages/gig/s1z_product_image_product_section.webp";
+const newArrFour = "https://vistapointe.net/images/product-wallpaper-8.jpg";
 
 const SplOfferData = [
-    {
-      _id: "201",
-      img: spfOne,
-      productName: "Cap for Boys",
-      price: "35.00",
-      color: "Blank and White",
-      badge: true,
-      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
-    },
-    {
-      _id: "202",
-      img: newArrFour,
-      productName: "Tea Table",
-      price: "180.00",
-      color: "Gray",
-      badge: true,
-      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
-    },
-    {
-      _id: "203",
-      img: spfThree,
-      productName: "Headphones",
-      price: "25.00",
-      color: "Mixed",
-      badge: true,
-      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
-    },
-    {
-      _id: "204",
-      img: spfFour,
-      productName: "Sun glasses",
-      price: "220.00",
-      color: "Black",
-      badge: true,
-      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
-    },
-  ];
+  {
+    _id: "201",
+    img: newArrOne,
+    productName: "Cap for Boys",
+    price: "35.00",
+    color: "Blank and White",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: "202",
+    img: newArrTwo,
+    productName: "Tea Table",
+    price: "180.00",
+    color: "Gray",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: "203",
+    img: newArrThree,
+    productName: "Headphones",
+    price: "25.00",
+    color: "Mixed",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: "204",
+    img: newArrFour,
+    productName: "Sun glasses",
+    price: "220.00",
+    color: "Black",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+];
 
 const ProductsOnSale = () => {
   return (
