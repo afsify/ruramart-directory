47ea49e32a2d794b326958bc4097490d893a0d58
AFSAL333 <afsalafs8008@gmail.com>
2025-01-08 23:38:02 +0530

    product info added

---
+++ client/src/components/user/product/ProductInfo.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+const ProductInfo = ({ productInfo }) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h2 className="text-4xl font-semibold">{productInfo.productName}</h2>
+      <p className="text-xl font-semibold">${productInfo.price}</p>
+      <p className="text-base text-gray-600">{productInfo.des}</p>
+      <p className="text-sm">Be the first to leave a review.</p>
+      <p className="font-medium text-lg">
+        <span className="font-normal">Colors:</span> {productInfo.color}
+      </p>
+      <button className="w-full py-4 bg-primeColor hover:bg-black duration-300 text-white text-lg font-titleFont">
+        Add to Cart
+      </button>
+      <p className="font-normal text-sm">
+        <span className="text-base font-medium"> Categories:</span> Spring
+        collection, Streetwear, Women Tags: featured SKU: N/A
+      </p>
+    </div>
+  );
+};
+
+ProductInfo.propTypes = {
+  productInfo: PropTypes.string,
+};
+
+export default ProductInfo;
