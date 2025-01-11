c16b0f24e4e91db804d770ee104864e3ef8dbc12
AFSAL333 <afsalafs8008@gmail.com>
2025-01-11 22:26:28 +0530

    signup page added

--- client/src/pages/user/SignUp.jsx
+++ client/src/pages/user/SignUp.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { BsCheckCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { logoLight } from "../../assets/images";
+import { BsCheckCircleFill } from "react-icons/bs";
 
 const SignUp = () => {
   // ============= Initial State Start here =============
@@ -133,9 +132,6 @@ const SignUp = () => {
     <div className="w-full h-screen flex items-center justify-start">
       <div className="w-1/2 hidden lgl:inline-flex h-full text-white">
         <div className="w-[450px] h-full bg-primeColor px-10 flex flex-col gap-6 justify-center">
-          <Link to="/">
-            <img src={logoLight} alt="logoImg" className="w-28" />
-          </Link>
           <div className="flex flex-col gap-1 -mt-1">
             <h1 className="font-titleFont text-xl font-medium">
               Get started for free
